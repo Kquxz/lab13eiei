da4794c9e15f434d51f46a6f6bf886518bf6db04
Kquxz <firstkungza2005@gmail.com>
2024-12-27 17:18:20 +0700

    lab13

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,37 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    for(int i=0 ; i < N ; i++){
+        B[0] += A[i];
+    }double uy = B[0]/N;
+    B[0] = uy;
+    
+    double io = 0.00;
+    for(int j=0; j < N; j++){
+        io += pow(A[j]-B[0],2);
+        B[1] = sqrt(io/N);
+    }
+
+    double pp = 1.00;
+    for(int k=0; k < N; k++){
+        pp *= A[k];
+        B[2] = pow(pp, 1.0 / N);
+    }
+    
+    double ty = 0.00;
+    for(int r=0; r < N; r++){
+        ty += 1.0 / A[r];
+        B[3] = N / ty;
+    }
+
+    double maxVal = 0;
+    double minVal = 1;
+    for(int o=0; o < N; o++){
+        if (A[o] > maxVal) maxVal = A[o];
+        if (A[o] < minVal) minVal = A[o];
+        B[4] = maxVal;
+        B[5] = minVal;
+    }
+}
